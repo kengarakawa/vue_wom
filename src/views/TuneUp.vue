a29289ccaf8466e1176cf1a89655f8affcf03e6b
Keng Arakawa <kengarakawa@gmail.com>
2021-10-27 18:14:00 +0700

    second commit

---
+++ src/views/TuneUp.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="container">
+    <h1 class="text-2xl font-bold text-center">Tune Up Stuff</h1>
+
+    <div class="flex bg-gray-600">
+      <aside class="bg-blue-300">
+        <li>Select Base Toon</li>
+      </aside>
+      <div class="output">Output here</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TuneUp",
+}
+</script>
+
+<style>
+h1 {
+  color: red;
+}
+</style>
